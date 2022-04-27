101750f4566e7301023c3ba0938105a60413a4ae
pabon <pabon@gmail.com>
2022-04-27 17:21:34 -0500

    pabon

---
+++ controlador.js
@@ -0,0 +1,113 @@
+
+let producto1={
+    nombre:"Ps4 Fat",
+    estado:"usado",
+    precio:"854000",
+    foto:""
+}
+let producto2={
+    nombre:"Ps4 Slim",
+    estado:"nuevo",
+    precio:"1120000",
+    foto:""
+}
+let producto3={
+    nombre:"Ps4 Pro",
+    estado:"nuevo",
+    precio:"1567700",
+    foto:""
+}
+let producto4={
+    nombre:"Xbox One",
+    estado:"usado",
+    precio:"784500",
+    foto:""
+}
+let producto5={
+    nombre:"Xbox One S",
+    estado:"usado",
+    precio:"988000",
+    foto:""
+}
+let producto6={
+    nombre:"Xbox One X",
+    estado:"Nuevo",
+    precio:"1643400",
+    foto:""
+}
+let producto7={
+    nombre:"Nintendo Switch",
+    estado:"usado",
+    precio:"1036600",
+    foto:""
+}
+let producto8={
+    nombre:"Xbox Series S",
+    estado:"nuevo",
+    precio:"1967000",
+    foto:""
+}
+let producto9={
+    nombre:"Xbox Series X",
+    estado:"nuevo",
+    precio:"2455000",
+    foto:""
+}
+let producto10={
+    nombre:"Ps5",
+    estado:"nuevo",
+    precio:"3150000",
+    foto:""
+}
+let producto11={
+    nombre:"Dualshock 4",
+    estado:"nuevo",
+    precio:"220000",
+    foto:""
+}
+let producto12={
+    nombre:"Dualshock 5",
+    estado:"nuevo",
+    precio:"384000",
+    foto:""
+}
+let producto13={
+    nombre:"Xbox One Controller",
+    estado:"nuevo",
+    precio:"218000",
+    foto:""
+}
+let producto14={
+    nombre:"Xbox Series Controller",
+    estado:"nuevo",
+    precio:"322000",
+    foto:""
+}
+let producto5={
+    nombre:"Switch Pro Controller",
+    estado:"nuevo",
+    precio:"312000",
+    foto:""
+}
+let productos=Array(
+producto1,
+producto2,
+producto3,
+producto4,
+producto5,
+producto6,
+producto7,
+producto8,
+producto9,
+producto10,
+producto11,
+producto12,
+producto13,
+producto14,
+producto15
+    )
+    console.log(productos)
+    productos.forEach(function(producto){
+        console.log(producto)
+        producto.nombre
+    })
